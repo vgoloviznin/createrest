b01958c9a55ba6aefad974f8deb7971649be5fae
Sergey Sova <i.am@lestad.net>
2017-05-29 15:23:15 +0300

    Add broken resources

--- examples/resources.js
+++ examples/resources.js
@@ -15,8 +15,48 @@ const ObjectController = {
   afterEach() {},
 }
 
-const routes = createRest(r => {
+const AuthController = {}
+
+const rou_tes = createRest(r => {
   r.resources('demo', ObjectController)
 })
+
+printRoutes(routes, true)
+
+
+const routes = createRest(r => {
+  r.before(filterUnsafe)
+  r.before(redirectByToken)
+
+  r.scope('auth', r => {
+    r.before(AuthController.redirectAuthenticated)
+
+    r.get(AuthController.getSession)
+    r.post(AuthController.createSession)
+    r.delete(AuthController.resetSession)
+
+    r.scope('recovery', r => {
+      r.before(AuthController.clearSessionCache)
+      r.post(AuthController.startPasswordRecovery)
+      r.post('send-email', AuthController.sendRecoveryEmail)
+      r.post('change-password', AuthController.changePasswordAfterRecovery)
+    })
+
+    r.delete('session', AuthController.checkSessions, AuthController.resetAllSessions)
+  })
+
+  r.resources('cards', CardsController, { noPatch: true }, r => {
+    r.before(CoreController.checkActivated('cards'))
+    r.get('trending', CardsController.getTrending)
+    r.get('latest', CardsController.updateCache, CardsController.getLatest)
+
+    r.get('featured', isAuthenticated, CardsController.getFeatured, Format.specify('cards-featured'))
+
+    r.member(r => {
+      r.get('rating', CardsController.getCardRating)
+    })
+  })
+})
+
 
 printRoutes(routes, true)
